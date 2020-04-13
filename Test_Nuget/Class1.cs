dd01e71342e11ccf9ddd1c4dc6c9e7970811ec34
rparsonsbb <richardparsons@blackbook.com>
2020-04-13 03:18:22 -0400

    first

---
+++ Test_Nuget/Class1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace Test_Nuget
+{
+    public class CommonUtils
+    {
+        public void DoOperation()
+        {
+            Console.WriteLine("Called DoOperation");
+            Console.WriteLine("Do it again");
+            Console.WriteLine("For the third time!");
+            Console.WriteLine("And the fourth!");
+            Console.WriteLine("And the fifth!");
+            Console.WriteLine("And the sixth!");
+            Console.WriteLine("And the seventh!");
+            Console.WriteLine("Create the 4th revision!");
+            Console.WriteLine("Another commit to tag beta");
+            Console.WriteLine("Commit after tag");
+            Console.WriteLine("Beta and tag");
+            Console.WriteLine("commit on master");
+            Console.WriteLine("tag on master");
+            Console.WriteLine("do a minor update");
+            Console.WriteLine("add a p atch");
+            Console.WriteLine("MAJOR VERSION");
+            Console.WriteLine("do a minor update");
+            Console.WriteLine("do a minor update");
+            Console.WriteLine("do a minor update");
+            Console.WriteLine("do a minor update");
+            Console.WriteLine("do a minor update");
+            Console.WriteLine("do a minor update");
+            Console.WriteLine("do a minor update");
+            Console.WriteLine("do a minor update");
+            Console.WriteLine("do a minor update");
+            Console.WriteLine("do a minor update");
+            Console.WriteLine("do a minor update");
+            Console.WriteLine("Another commit to tag beta");
+            Console.WriteLine("Commit after tag");
+            Console.WriteLine("Beta and tag");
+            Console.WriteLine("commit on master");
+            Console.WriteLine("tag on master");
+            Console.WriteLine("do a minor update");
+            Console.WriteLine("add a p atch");
+            Console.WriteLine("MAJOR VERSION");
+            Console.WriteLine("do a minor update");
+            Console.WriteLine("Beta and tag");
+            Console.WriteLine("commit on master");
+            Console.WriteLine("tag on master");
+            Console.WriteLine("do a minor update");
+            Console.WriteLine("add a p atch");
+            Console.WriteLine("MAJOR VERSION");
+            Console.WriteLine("do a minor update");
+            Console.WriteLine("add a p atch");
+            Console.WriteLine("MAJOR VERSION");
+            Console.WriteLine("do a minor update");
+            Console.WriteLine("do a minor update");
+            Console.WriteLine("do a minor update");
+            Console.WriteLine("Another commit to tag beta");
+            Console.WriteLine("Commit after tag");
+            Console.WriteLine("Beta and tag");
+            Console.WriteLine("commit on master");
+            Console.WriteLine("tag on master");
+            Console.WriteLine("do a minor update");
+            Console.WriteLine("add a p atch");
+            Console.WriteLine("MAJOR VERSION");
+            Console.WriteLine("do a minor update");
+            Console.WriteLine("Beta and tag");
+            Console.WriteLine("commit on master");
+            Console.WriteLine("tag on master");
+            Console.WriteLine("do a minor update");
+            Console.WriteLine("add a p atch");
+            Console.WriteLine("MAJOR VERSION");
+            Console.WriteLine("do a minor update");
+            Console.WriteLine("add a p atch");
+            Console.WriteLine("MAJOR VERSION");
+            Console.WriteLine("do a minor update");
+        }
+    }
+}
